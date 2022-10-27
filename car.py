8e2dddc201628df8a2329ebd53a7d6734a35a592
coderbarn <coderbarn@users.noreply.github.com>
2022-10-27 08:36:23 -0400

    Initial version of file

---
+++ car.py
@@ -0,0 +1,13 @@
+
+
+from distutils.archive_util import make_archive
+
+
+class Car:
+    def __init__(self, year:int, model:str):
+        self.year = year
+        self.model = model
+        
+if __name__ == "__main__":
+    car = Car(2000, "328D")
+    
